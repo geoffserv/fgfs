65b64b7e0bf162cfc6c88703b268979e5b330123
Geoffserv <gtraugott@gmail.com>
2020-05-02 14:05:53 -0500

    FGFSMk2RevB and Jaminator Code

---
+++ jaminator_buttonarray_usb_2020-02-26-01_midi/jaminator_buttonarray_usb_2020-02-26-01_midi.ino
@@ -0,0 +1,91 @@
+/* Geoff's button matrix MIDI controller Teensy code
+  I stole lots of logic from Teensy USB-MIDI Buttons example code by Liam Lacey
+
+  Using a switch matrix design I found at https://forum.arduino.cc/index.php?topic=253215.0
+  Schematic https://forum.arduino.cc/index.php?action=dlattach;topic=253215.0;attach=87528
+
+  You must select MIDI from the "Tools > USB Type" menu for this code to compile.
+
+  To change the name of the USB-MIDI device, edit the STR_PRODUCT define
+  in the /Applications/Arduino.app/Contents/Java/hardware/teensy/avr/cores/usb_midi/usb_private.h
+  file. You may need to clear your computers cache of MIDI devices for the name change to be applied.
+
+  See https://www.pjrc.com/teensy/td_midi.html for the Teensy MIDI library documentation.
+
+*/
+
+const byte NUM_OF_X_PINS = 8;
+const byte NUM_OF_Y_PINS = 4;
+const byte SWITCHPINS_X[NUM_OF_X_PINS] = { 4, 5, 6, 7,  8, 9, 10 };  // pins to which the x row is connected
+const byte SWITCHPINS_Y[NUM_OF_Y_PINS] = { 0, 1, 2, 3 }; // pins to which the y col is connected
+const byte NUM_OF_SWITCHES = NUM_OF_X_PINS * NUM_OF_Y_PINS ; // number of buttons
+
+byte switchValue[NUM_OF_SWITCHES];
+
+// the MIDI channel number to send messages
+const int MIDI_CHAN = 1;
+
+const int MIDI_NOTE_NUMS[NUM_OF_SWITCHES] = { 0, 41, 42, 43, 36, 37, 38, 39, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67};
+
+void setup() {
+
+  Serial.begin(9600); // DEBUG: USB is always 12Mbit/sec no matter the baud rate here
+
+  for (byte i = 0; i < NUM_OF_SWITCHES; i++) {
+    switchValue[i] = 1; // set all switches to 'off' to start with (1 is off in this logic)
+  }
+  
+  for (byte i = 0; i < NUM_OF_X_PINS; i++) { // Configure the x pins for input mode with pullup resistors.
+    pinMode(SWITCHPINS_X[i], INPUT_PULLUP);
+  }
+
+  for (byte i = 0; i < NUM_OF_Y_PINS; i++) { // Configure the y pins for input mode with pullup resistors.
+    pinMode(SWITCHPINS_Y[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  
+  byte n = 0; // This is used to enumerate every actual button starting from switch 0
+  
+  for (byte xpin = 0; xpin < NUM_OF_X_PINS; xpin++) { // Loop through every digital x pin
+    
+    pinMode(SWITCHPINS_X[xpin], OUTPUT); // Sets the digital x pin as output
+    
+    digitalWrite(SWITCHPINS_X[xpin], LOW); // 0 volts (https://www.arduino.cc/en/Reference/DigitalWrite)
+    
+    for (byte ypin = 0; ypin < NUM_OF_Y_PINS; ypin++) { // Loop through every digital y pin
+      
+      // At this point, we are examining "button" n
+      // I've noticed that 1 = button unpressed, 0 = button pressed
+        
+      byte switchCurrentValue = digitalRead(SWITCHPINS_Y[ypin]); // Read Y Pin and set CurrentValue of the Switch. 1 = off, 0 = on
+        
+      if ((switchValue[n] != switchCurrentValue)) { // If there's an edge change ) { // Oh button n's state has changed from our last recorded state!
+        switchValue[n] = switchCurrentValue; // Record the new value
+
+        char buf[22];
+        sprintf(buf, "Button %d value is %d", n+1, switchValue[n]);
+        Serial.println(buf);
+        
+        if (switchCurrentValue == 0) { // Switch ON!
+          usbMIDI.sendNoteOn (MIDI_NOTE_NUMS[n+1], 110, MIDI_CHAN);
+        }
+        else { // Switch OFF!
+          usbMIDI.sendNoteOff (MIDI_NOTE_NUMS[n+1], 0, MIDI_CHAN);
+        }
+      }
+      
+      n++; // move on to the next button
+      
+      delay(1); // 1 ms between each button, just chill out OK
+      
+    }
+    pinMode(SWITCHPINS_X[xpin], INPUT_PULLUP); // Stop the digitalWrite
+  }
+  
+  while (usbMIDI.read())
+  {
+    // ignoring incoming messages, so don't do anything here.
+  }
+}
